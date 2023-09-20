83a72b4ffc912f9e59e0355984c0ff23e998694f
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-09-20 11:10:01 -0600

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -38,4 +38,10 @@ router.post('/', async (req, res, next) => {
   return res.json({ user: safeUser });
 });
 
+// Log out
+router.delete('/', (_req, res) => {
+  res.clearCookie('token');
+  return res.json({ message: 'success' });
+});
+
 module.exports = router;
