ca177d27594c44697ada415fc389181138859fb3
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-09-27 18:22:16 -0600

    added the endpoint to get all spots: /api/spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,6 +2,12 @@ const router = require('express').Router()
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { Spot } = require('../../db/models')
+const { Op } = require('sequelize')
+
+router.get('/', async (req, res) => {
+  const spots = await Spot.findAll()
+  return res.status(200).json({ spots })
+})
 
 
 
