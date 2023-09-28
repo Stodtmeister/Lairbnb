e50a97d20e04d695b00522a7c5744e1d595a66ed
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-09-28 10:53:35 -0600

    added the endpoint to the all spots owned by the current user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,12 +1,22 @@
 const router = require('express').Router()
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
-const { Spot } = require('../../db/models')
+const { Spot, User } = require('../../db/models')
 const { Op } = require('sequelize')
+const { requireAuth } = require('../../utils/auth')
 
 router.get('/', async (req, res) => {
   const spots = await Spot.findAll()
   return res.status(200).json({ spots })
+})
+
+router.get('/current', requireAuth, async (req, res) => {
+  const { user: currentUser } = req
+  const spots = await Spot.findAll({
+    where: { ownerId: currentUser.id }
+  })
+
+  res.status(200).json({ spots })
 })
 
 
