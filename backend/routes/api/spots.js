adab3e126902ac35acc9baff27529a1dfe1b8529
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-09-27 18:12:22 -0600

    initialized spots router

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,3 +1,8 @@
 const router = require('express').Router()
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const { Spot } = require('../../db/models')
+
+
 
 module.exports = router
