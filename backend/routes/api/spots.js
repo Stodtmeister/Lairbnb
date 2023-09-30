b858c088217e3eaa828432c511aa04dc75a506e8
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-09-30 13:13:31 -0600

    fixed the GET api/spots endpoint to include avgRating and previewImage

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -5,8 +5,15 @@ const { Spot, User, Image, Review, sequelize } = require('../../db/models')
 const { Op } = require('sequelize')
 const { requireAuth } = require('../../utils/auth')
 
+
 router.get('/', async (req, res) => {
-  const spots = await Spot.findAll()
+  const spots = await Spot.findAll(
+    {include: [{ model: Review }, { model: Image }] }
+  )
+
+
+  const updatedSpots = getAvgRating(spots)
+  return res.json({ Spots: updatedSpots})
   return res.status(200).json({ spots })
 })
 
@@ -15,6 +22,7 @@ router.get('/current', requireAuth, async (req, res) => {
   const spots = await Spot.findAll({
     where: { ownerId: currentUser.id }
   })
+
 
   return res.status(200).json({ spots })
 })
@@ -64,6 +72,40 @@ router.get('/:spotId', async (req, res) => {
   if (!spot) return res.status(404).json({ message: "Spot couldn't be found"})
   return res.status(200).json(review)
 })
+
+function getAvgRating(arr) {
+  const newArr = []
+
+  for (let spot of arr) {
+    let count = 0
+    let totalStars = 0
+    let previewImage
+    let avgRating
+
+    for (let review of spot.Reviews) {
+      count++
+      totalStars += review.stars
+    }
+
+    for (let images of spot.Images) {
+      if (images.preview === true) {
+        preview = images.url
+        // spot.dataValues.previewImage = images.url
+        break
+      }
+
+      previewImage = 'Upload preview image'
+      // spot.dataValues.previewImage = 'Upload preview image'
+    }
+
+    avgRating = totalStars / count
+    // spot.dataValues.avgRating = totalStars / count
+    const { id, ownerId, address, city, state, country, lat, lng, name, description, price, createdAt, updatedAt } = spot
+    newArr.push({ id, ownerId, address, city, state, country, lat, lng, name, description, price, createdAt, updatedAt, avgRating, previewImage })
+  }
+
+  return newArr
+}
 
 
 
