07f74ccd88d366109183777e43e0243a9a531937
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-10-02 13:49:30 -0600

    added endpoint to get all of the current users bookings

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,3 +1,21 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const { Spot, User, Image, Review, Booking } = require('../../db/models');
+const { Op } = require('sequelize');
+const { requireAuth } = require('../../utils/auth');
+
+router.get('/current', requireAuth, async (req, res) => {
+  console.log('hi')
+  const bookings = await Booking.findAll({
+    where: { userId: req.user.id },
+    include: {
+      model: Spot,
+      attributes: { exclude: ['description', 'createdAt', 'updatedAt'] },
+    }
+  })
+
+  return res.status(200).json({ Bookings: bookings })
+})
 
 module.exports = router
