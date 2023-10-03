f435c769db4cd634b63b96d4a6346150b0536335
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-10-03 17:29:09 -0600

    added reviewImagesRouter and spotImagesRouter

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,7 +4,8 @@ const usersRouter = require('./users.js');
 const bookingsRouter = require('./bookings.js');
 const reviewsRouter = require('./reviews.js')
 const spotsRouter = require('./spots.js')
-const spotImagesRouter = require('.')
+const spotImagesRouter = require('./spot-images.js')
+const reviewImagesRouter = require('./review-images.js')
 
 const { restoreUser } = require("../../utils/auth.js");
 
@@ -18,7 +19,8 @@ router.use('/users', usersRouter);
 router.use('/bookings', bookingsRouter);
 router.use('/reviews', reviewsRouter);
 router.use('/spots', spotsRouter);
-router.use('/spot-images', )
+router.use('/spot-images', spotImagesRouter);
+router.use('/review-images', reviewImagesRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
