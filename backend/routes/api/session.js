d0e6a10763006933e2e8e6ef6122e9e0c2caf7f5
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-10-03 17:53:20 -0600

    added firstName and lastName to get /session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -65,6 +65,8 @@ router.get('/', (req, res) => {
   if (user) {
     const safeUser = {
       id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
       email: user.email,
       username: user.username,
     };
