62e7d20a9b2eb2ed67e4676ef520ff527ae25d50
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-10-03 17:41:19 -0600

    formatting

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
-const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
-const { Spot, User, Image, Review, sequelize } = require('../../db/models');
-const { Op } = require('sequelize');
+const { Spot, Image } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth');
 
 // Delete a review image
-router.delete('/review-images/:imageId', requireAuth, async (req, res, next) => {
+router.delete('/:imageId', requireAuth, async (req, res, next) => {
   const image = await Image.findByPk(req.params.imageId, { include: Spot });
 
   if (!image) return res.status(404).json({ message: "Spot Image couldn't be found" })
