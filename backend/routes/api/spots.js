6df6b5de96fe828a3cb31f3395e8fff570652e09
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-10-03 13:08:09 -0600

    added endpoint to delete a spot image

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -269,30 +269,26 @@ router.post('/:spotId/bookings',
 
 // Delete a spot image
 router.delete('/:spotId/images/:imageId', requireAuth, async (req, res, next) => {
-  const image = await Image.findByPk(req.params.imageId, {
-    include: {
-      model: Spot,
-      attributes: ['ownerId']
-    }
-  })
+  const image = await Image.findByPk(req.params.imageId, { include: Spot });
 
-  // const spot = await image.getSpot()
   if (!image) return res.status(404).json({ message: "Spot Image couldn't be found" })
-  if (image.Spot) authorization(image.Spot, req.user, next)
 
-  // if (!spot) return res.status(404).json({ message: "Spot couldn't be found" })
-  // authorization(spot, req.user, next)
+  //? (Weird authorization error) authorization(image.Spot, req.user, next)
+  if (req.user.id !== image.Spot.ownerId) {
+    let err = new Error('Forbidden')
+    err.status = 403
+    return next(err)
+  }
 
-
-  return res.json(image.Spot)
-
+  await image.destroy()
+  return res.status(200).json({ message: 'Successfully deleted' })
 })
 
 function authorization(spot, user, next) {
   if (spot.ownerId !== user.id) {
     let err = new Error('Forbidden')
     err.status = 403
-    err.title = 'Require proper authorization'
+    // err.title = 'Require proper authorization'
     next(err)
   }
 }
