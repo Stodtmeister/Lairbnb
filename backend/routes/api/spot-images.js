ffd68e4dbca0af89a69d3dccbfa1d75fb428e58a
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-10-04 11:27:14 -0600

    updated DELETE review-images/:imageId and spot-images/:spotId

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -7,9 +7,8 @@ router.delete('/:imageId', requireAuth, async (req, res, next) => {
   const image = await Image.findByPk(req.params.imageId, { include: Spot });
 
   if (!image) return res.status(404).json({ message: "Spot Image couldn't be found" })
-
-  //? (Weird authorization error) authorization(image.Spot, req.user, next)
-  if (req.user.id !== image.Spot.ownerId) {
+  
+  if (req.user.id !== image.dataValues.id) {
     let err = new Error('Forbidden')
     err.status = 403
     return next(err)
