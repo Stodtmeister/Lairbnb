dfbe77437bbf0d29739ed6db282b707475eafa23
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-10-04 13:08:21 -0600

    added err.title to review-images and spot-images

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -7,9 +7,10 @@ router.delete('/:imageId', requireAuth, async (req, res, next) => {
   const image = await Image.findByPk(req.params.imageId, { include: Spot });
 
   if (!image) return res.status(404).json({ message: "Spot Image couldn't be found" })
-  
+
   if (req.user.id !== image.dataValues.id) {
     let err = new Error('Forbidden')
+    err.title = 'Require proper authorization'
     err.status = 403
     return next(err)
   }
