f72b184c904acfcab6c536d07f23e00ae599d78a
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-10-05 10:24:01 -0600

    Cleaned up code in index.js

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,14 +6,9 @@ const reviewsRouter = require('./reviews.js')
 const spotsRouter = require('./spots.js')
 const spotImagesRouter = require('./spot-images.js')
 const reviewImagesRouter = require('./review-images.js')
-
 const { restoreUser } = require("../../utils/auth.js");
 
-// Connect restoreUser middleware to the API router
-  // If current user session is valid, set req.user to the user in the database
-  // If current user session is not valid, set req.user to null
 router.use(restoreUser);
-
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/bookings', bookingsRouter);
