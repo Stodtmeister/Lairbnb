e29f91bdc904146f11da44e1271343d51bfcfa6a
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-10-05 12:57:36 -0600

    Fixed validations in review.js Model

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -30,7 +30,9 @@ validateSpot = [
 validateReview = [
   check('review').exists({ checkFalsy: true }).notEmpty()
     .withMessage('Review text is required'),
-  check('stars').exists({ checkFalsy: true }).notEmpty().isIn([1, 2, 3, 4, 5])
+  check('stars').exists({ checkFalsy: true })
+    .notEmpty()
+    .isIn([1, 2, 3, 4, 5])
     .withMessage('Stars must be an integer from 1 to 5'),
   handleValidationErrors,
 ];
