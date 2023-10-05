5272b39e0bb65633f13a64dd7594c0db1e3e7873
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-10-05 10:29:21 -0600

    Cleaned up code in reviews.js

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -6,17 +6,12 @@ const { Op } = require('sequelize');
 const { requireAuth } = require('../../utils/auth');
 
 validateReview = [
-  check('review')
-    .exists({ checkFalsy: true })
-    .notEmpty()
+  check('review').exists({ checkFalsy: true }).notEmpty()
     .withMessage('Review text is required'),
-  check('stars')
-    .exists({ checkFalsy: true })
-    .notEmpty()
-    .isIn([1, 2, 3, 4, 5])
+  check('stars').exists({ checkFalsy: true }).notEmpty().isIn([1, 2, 3, 4, 5])
     .withMessage('Stars must be an integer from 1 to 5'),
-  handleValidationErrors
-]
+  handleValidationErrors,
+];
 
 // Get all reviews of the current user
 router.get('/current', requireAuth, async (req, res, next) => {
@@ -29,81 +24,69 @@ router.get('/current', requireAuth, async (req, res, next) => {
     ],
   });
 
-  // reviews.forEach(review => {
-  //   console.log(review.ReviewImages)
-  //   // const previewImg = review.Spot.previewImage
-  //   // const spotImages = review.Spot.SpotImages
-  //   // if (!previewImg) {
-  //   //   if (spotImages.length) {
-  //   //     for (let images of spotImages) {
-  //   //       if (images.preview === true) {
-  //   //         previewImg = images.url
-  //   //         break
-  //   //       }
-  //   //     }
-  //   //   } else {
-  //   //     previewImg = 'Upload preview image'
-  //   //   }
-  //   // }
-  // })
-  if (!reviews) return res.status(404).json({ message: 'No reviews yet' })
+  if (!reviews)
+    return res.status(404).json({ message: 'No reviews yet' });
   return res.status(200).json({ Reviews: reviews });
 });
 
 // Add an image to a review based on the review's id
 router.post('/:reviewId/images', requireAuth, async (req, res, next) => {
   const review = await Review.findByPk(req.params.reviewId, {
-    include: {
-      model: Image, as: 'ReviewImages',
-      attributes: []
-    },
-    attributes: [ 'userId', [sequelize.fn('COUNT', sequelize.col('ReviewImages.id')), 'imageCount'] ]
-  })
+    include: { model: Image, as: 'ReviewImages', attributes: [] },
+    attributes: [ 'userId', [sequelize.fn('COUNT', sequelize.col('ReviewImages.id')), 'imageCount'] ],
+  });
 
-  if (!review.dataValues.userId) return res.status(404).json({ message: "Review couldn't be found" })
-  if (!authorization(review, req.user, next)) return
+  if (!review.dataValues.userId)
+    return res.status(404).json({ message: "Review couldn't be found" });
+  if (!authorization(review, req.user, next))
+    return;
 
-  const imageCount = review.dataValues.imageCount
-  if (imageCount >= 10) return res.status(403).json({ message: 'Maximum number of images for this resource was reached'})
+  const imageCount = review.dataValues.imageCount;
+  if (imageCount >= 10)
+    return res.status(403).json({ message: 'Maximum number of images for this resource was reached' });
 
-  const imageableType = 'Review'
-  const imageableId = req.params.reviewId
-  const image = await Image.create({ imageableType, imageableId, ...req.body})
-  const { id, url } = image
-  return res.status(200).json({ id, url })
-})
+  const imageableType = 'Review';
+  const imageableId = req.params.reviewId;
+  const image = await Image.create({ imageableType, imageableId, ...req.body });
+  const { id, url } = image;
+  return res.status(200).json({ id, url });
+});
 
 // Edit a review
 router.put('/:reviewId', requireAuth, validateReview, async (req, res, next) => {
-  const review = await Review.findByPk(req.params.reviewId)
+  const review = await Review.findByPk(req.params.reviewId);
 
-  if (!review) return res.status(404).json({ message: "Review couldn't be found" })
-  if (!authorization(review, req.user, next)) return
+  if (!review)
+    return res.status(404).json({ message: "Review couldn't be found" });
+  if (!authorization(review, req.user, next))
+    return;
 
-  const updatedReview = await review.update(req.body)
-  return res.json(updatedReview)
-})
+  const updatedReview = await review.update(req.body);
+  return res.json(updatedReview);
+});
 
 // Delete a review
 router.delete('/:reviewId', requireAuth, async (req, res, next) => {
-  const review = await Review.findByPk(req.params.reviewId)
-  if (!review) return res.status(404).json({ message: "Review couldn't be found" })
-  if (!authorization(review, req.user, next)) return
+  const review = await Review.findByPk(req.params.reviewId);
+  if (!review)
+    return res.status(404).json({ message: "Review couldn't be found" });
+  if (!authorization(review, req.user, next))
+    return;
 
-  await review.destroy()
-  return res.status(200).json({ message: 'Successfully deleted' })
-})
+  await review.destroy();
+  return res.status(200).json({ message: 'Successfully deleted' });
+});
 
 function authorization(review, user, next) {
   if (review.userId !== user.id) {
-    let err = new Error('Forbidden')
-    err.status = 403
-    err.title = 'Require proper authorization'
-    next(err)
-    return false
+    let err = new Error('Forbidden');
+    err.status = 403;
+    err.title = 'Require proper authorization';
+    next(err);
+    return false;
   }
 
-  return true
+  return true;
 }
 
 module.exports = router;
