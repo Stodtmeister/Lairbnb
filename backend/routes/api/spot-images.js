bb701034b87eafc231cf1c1753d55631261d77f7
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-10-05 10:32:26 -0600

    Cleaned up code in spot-images.js

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -6,17 +6,18 @@ const { requireAuth } = require('../../utils/auth');
 router.delete('/:imageId', requireAuth, async (req, res, next) => {
   const image = await Image.findByPk(req.params.imageId, { include: Spot });
 
-  if (!image) return res.status(404).json({ message: "Spot Image couldn't be found" })
+  if (!image)
+    return res.status(404).json({ message: "Spot Image couldn't be found" });
 
   if (req.user.id !== image.dataValues.id) {
-    let err = new Error('Forbidden')
-    err.title = 'Require proper authorization'
-    err.status = 403
-    return next(err)
+    let err = new Error('Forbidden');
+    err.title = 'Require proper authorization';
+    err.status = 403;
+    return next(err);
   }
 
-  await image.destroy()
-  return res.status(200).json({ message: 'Successfully deleted' })
-})
+  await image.destroy();
+  return res.status(200).json({ message: 'Successfully deleted' });
+});
 
 module.exports = router;
