8919b037c24bd71ebf99e746f3b719e8ae1b6046
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-10-05 22:28:06 -0600

    formatting

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -147,8 +147,9 @@ router.post('/:spotId/images', requireAuth, async (req, res, next) => {
   if (!authorization(spot, req.user, next))
     return;
 
+  let previewImg;
   if (req.body.preview) {
-    const previewImg = req.body.url;
+    previewImg = req.body.url;
   }
 
   await spot.update({ previewImage: previewImg });
