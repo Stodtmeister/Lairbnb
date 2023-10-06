3d46023b082af55fa968aff2ea8c5d0c6027673e
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-10-05 23:34:51 -0600

    fixed DELETE api/review-images.js

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
-const { Spot, Image } = require('../../db/models');
+const { Review, Image } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth');
 
 // Delete a review image
 router.delete('/:imageId', requireAuth, async (req, res, next) => {
-  const image = await Image.findByPk(req.params.imageId, { include: Spot });
+  const image = await Image.findByPk(req.params.imageId, { include: Review });
 
   if (!image)
     return res.status(404).json({ message: "Review Image couldn't be found" });
 
-  if (req.user.id !== image.dataValues.id) {
+  if (req.user.id !== image.Review.id) {
     let err = new Error('Forbidden');
     err.title = 'Require proper authorization';
     err.status = 403;
