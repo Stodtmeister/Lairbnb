3ba66f7c2b7e7fedc9c5b17695060a9b23f501ee
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-11-05 01:53:29 -0700

    removed the vite frontend

--- frontendVITE/src/components/index.js
+++
@@ -1,2 +0,0 @@
-export { default as LoginFormPage } from './LoginFormPage/LoginFormPage.jsx'
-export { default as SignupFormPage } from './SignupFormPage/SignupFormPage.jsx'
--- frontendVITE/src/router.jsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from 'react-router-dom'
-import App from './App'
-import { LoginFormPage, SignupFormPage } from './components'
-
-export const router = createBrowserRouter([
-  { path: '/', element: <App /> },
-  { path: '/login', element: <LoginFormPage /> },
-  { path: '/signup', element: <SignupFormPage /> }
-])
