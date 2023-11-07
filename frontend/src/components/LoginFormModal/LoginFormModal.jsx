c4dfcff53ad69b86dff176d7ffd9b882b5891fa7
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-11-06 23:53:39 -0700

    Working on signup validations

--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
 import './LoginForm.css'
