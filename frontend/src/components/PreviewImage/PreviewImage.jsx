8301db68eb031870097bf7815e62fa6bf2f14537
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-11-07 23:14:50 -0700

    formatting

--- frontend/src/components/PreviewImage/PreviewImage.jsx
+++ frontend/src/components/PreviewImage/PreviewImage.jsx
@@ -1,9 +1,6 @@
-import { Redirect } from 'react-router-dom'
 import './PreviewImage.css'
-import Spot from '../../pages/Spot/Spot'
 
 export default function PreviewImage({ avgRating, id, address, city, state, previewImage, price, name }) {
-
   return (
     <div className="spot" >
       <img src={previewImage} alt="House" />
