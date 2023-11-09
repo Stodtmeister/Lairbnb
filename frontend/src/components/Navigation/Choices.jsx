67e009505760a45b135e7223f953f1fde558cd21
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-11-09 11:38:01 -0700

    added styling to spots grid

--- frontend/src/components/Navigation/Choices.jsx
+++ frontend/src/components/Navigation/Choices.jsx
@@ -1,3 +1,3 @@
 export default function Choices() {
-  return <h2>Choices</h2>
+  return <h2>{''}</h2>
 }
--- frontend/src/components/PreviewImage/PreviewImage.jsx
+++ frontend/src/components/PreviewImage/PreviewImage.jsx
@@ -6,8 +6,8 @@ export default function PreviewImage({ avgRating, city, state, previewImage, pri
     <div className="spot" >
       <img src='https://a0.muscache.com/im/pictures/prohost-api/Hosting-53402672/original/1085a9f4-a962-47ac-994b-7b8b356ace47.jpeg?im_w=720' alt="House" />
       <div className="spot-footer">
-        <span>{city}, {state} <i class="fa-sharp fa-solid fa-star"></i>{avgRating}</span>
-        <span>{price}</span>
+        <span class='bold'>{city}, {state} <i class="fa-sharp fa-solid fa-star"></i>{avgRating.toPrecision(2)}</span>
+        <span class='bold'>${price}<span className='not-bold'>night</span></span>
       </div>
     </div>
   )
