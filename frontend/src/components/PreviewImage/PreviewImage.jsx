779745c22f063f7afa5c3da63a35b10356002ffb
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-11-09 14:47:22 -0700

    redirect user to spot page when clicking on a spot

--- frontend/src/components/PreviewImage/PreviewImage.jsx
+++ frontend/src/components/PreviewImage/PreviewImage.jsx
@@ -1,9 +1,15 @@
+import { useHistory } from 'react-router-dom'
 import './PreviewImage.css'
 
 export default function PreviewImage({ id, name, avgRating, city, state, previewImage, price }) {
+  const history = useHistory()
+
+  function handleClick() {
+    history.push(`/spot/${id}`)
+  }
   // previewImage = require('../../images/sajad-nori-s1puI2BWQzQ-unsplash.jpg')
   return (
-    <div className="spot tooltip" >
+    <div className="spot tooltip" onClick={handleClick}>
       <span className='tooltiptext'>{name}</span>
       <img src='https://a0.muscache.com/im/pictures/prohost-api/Hosting-53402672/original/1085a9f4-a962-47ac-994b-7b8b356ace47.jpeg?im_w=720' alt="House" />
       <div className="spot-footer">
