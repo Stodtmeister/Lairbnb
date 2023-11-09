2095f8c890f764015eac003776dd6239127b25ea
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-11-09 11:25:23 -0700

    added styling to spots grid

--- frontend/src/components/PreviewImage/PreviewImage.jsx
+++ frontend/src/components/PreviewImage/PreviewImage.jsx
@@ -1,14 +1,13 @@
 import './PreviewImage.css'
 
-export default function PreviewImage({ avgRating, id, address, city, state, previewImage, price, name }) {
+export default function PreviewImage({ avgRating, city, state, previewImage, price }) {
+  // previewImage = require('../../images/sajad-nori-s1puI2BWQzQ-unsplash.jpg')
   return (
     <div className="spot" >
-      <img src={previewImage} alt="House" />
+      <img src='https://a0.muscache.com/im/pictures/prohost-api/Hosting-53402672/original/1085a9f4-a962-47ac-994b-7b8b356ace47.jpeg?im_w=720' alt="House" />
       <div className="spot-footer">
-        <p>{city}, {state}</p>
-        <p>{price}</p>
-
-        {avgRating}
+        <span>{city}, {state} <i class="fa-sharp fa-solid fa-star"></i>{avgRating}</span>
+        <span>{price}</span>
       </div>
     </div>
   )
