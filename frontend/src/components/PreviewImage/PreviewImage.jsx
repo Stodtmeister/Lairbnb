cc49832606c3a38b564304f980e693d7136b3819
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-11-09 14:08:40 -0700

    added a tooltip when hovering over a spot

--- frontend/src/components/PreviewImage/PreviewImage.jsx
+++ frontend/src/components/PreviewImage/PreviewImage.jsx
@@ -1,9 +1,10 @@
 import './PreviewImage.css'
 
-export default function PreviewImage({ avgRating, city, state, previewImage, price }) {
+export default function PreviewImage({ id, name, avgRating, city, state, previewImage, price }) {
   // previewImage = require('../../images/sajad-nori-s1puI2BWQzQ-unsplash.jpg')
   return (
-    <div className="spot" >
+    <div className="spot tooltip" >
+      <span className='tooltiptext'>{name}</span>
       <img src='https://a0.muscache.com/im/pictures/prohost-api/Hosting-53402672/original/1085a9f4-a962-47ac-994b-7b8b356ace47.jpeg?im_w=720' alt="House" />
       <div className="spot-footer">
         <span class='bold'>{city}, {state} <i class="fa-sharp fa-solid fa-star"></i>{avgRating.toPrecision(2)}</span>
