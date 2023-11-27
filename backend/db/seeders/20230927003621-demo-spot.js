77d488e356cd7f9acc5a0459b2fd50c8986c80a0
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-11-27 10:14:33 -0700

    added to the seeder files

--- backend/db/seeders/20230927003621-demo-spot.js
+++ backend/db/seeders/20230927003621-demo-spot.js
@@ -1,10 +1,9 @@
 'use strict';
-
 const { Spot } = require('../models')
 
 let options = {};
 if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define the schema in options object
+  options.schema = process.env.SCHEMA;
 }
 
 module.exports = {
@@ -114,7 +113,110 @@ module.exports = {
         price: 533,
         previewImage: 'https://source.unsplash.com/XJnP4L958ds',
       },
-
+      {
+        ownerId: 1,
+        address: '123 Meadow Lane',
+        city: 'Chicago',
+        state: 'Illinois',
+        country: 'United States',
+        lat: 12.4,
+        lng: 7.3,
+        name: 'Tree House',
+        description: 'Tree house in downtown Chicago',
+        price: 999,
+        previewImage: 'https://source.unsplash.com/2MA8dFvOMec'
+      },
+      {
+        ownerId: 2,
+        address: '1 Bagshot Row',
+        city: 'Hobbiton',
+        state: 'Shire',
+        country: 'Middle-earth',
+        lat: 29.56,
+        lng: 32.1,
+        name: 'Bag End',
+        description: 'In a hole in the ground there lived a hobbit. Not a nasty, dirty, wet hole, filled with the ends of worms and an oozy smell, nor yet a dry, bare, sandy hole with nothing in it to sit down on or to eat: it was a hobbit-hole, and that means comfort.',
+        price: 7000,
+        previewImage: 'https://source.unsplash.com/zeW9BQbWmJs'
+      },
+      {
+        ownerId: 3,
+        address: '99 Alcatraz blvd',
+        city: 'San Francisco',
+        state: 'California',
+        country: 'United States',
+        lat: 63.1,
+        lng: 10.7,
+        name: 'Alcatraz prison',
+        description: 'Cell #62 in Alcatraz prison.',
+        price: 93,
+        previewImage: 'https://source.unsplash.com/eWqOgJ-lfiI',
+      },
+      {
+        ownerId: 4,
+        address: '45 Numenor Blvd',
+        city: 'Minas Tirith',
+        state: 'Gondor',
+        country: 'Middle-earth',
+        lat: 66,
+        lng: 18.7,
+        name: 'The White City',
+        description: 'The throne room',
+        price: 533,
+        previewImage: 'https://source.unsplash.com/0yfWDwHOB0g',
+      },
+      {
+        ownerId: 1,
+        address: '45 S 5000 W',
+        city: 'Seattle',
+        state: 'Washington',
+        country: 'United States',
+        lat: 66,
+        lng: 18.7,
+        name: 'Mansion',
+        description: 'A big house',
+        price: 533,
+        previewImage: 'https://source.unsplash.com/fsUVFq7hUgg',
+      },
+      {
+        ownerId: 2,
+        address: '87th Main St',
+        city: 'Boise',
+        state: 'Idaho',
+        country: 'United States',
+        lat: 66,
+        lng: 18.7,
+        name: 'Farm',
+        description: 'A farm house in Idaho',
+        price: 19,
+        previewImage: 'https://source.unsplash.com/F4zubUaI24w',
+      },
+      {
+        ownerId: 3,
+        address: '98 Moon Blvd',
+        city: 'Dallas',
+        state: 'Texas',
+        country: 'United States',
+        lat: 66,
+        lng: 18.7,
+        name: 'Shack',
+        description: 'A cozy shack',
+        price: 1000,
+        previewImage: 'https://source.unsplash.com/nkOLtHRZpGQ',
+      },
+      {
+        ownerId: 4,
+        address: '99 Pelican Way',
+        city: 'Orlando',
+        state: 'Florida',
+        country: 'United States',
+        lat: 66,
+        lng: 18.7,
+        name: 'Beach House',
+        description: 'A beach house in Orlando',
+        price: 533,
+        previewImage: 'https://source.unsplash.com/XJnP4L958ds',
+      },
     ], { validate: true })
   },
 
