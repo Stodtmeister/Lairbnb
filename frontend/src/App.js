3e9165862625345f46dccbdb00baca21c2ade601
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-11-26 22:57:09 -0700

    added styling to home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Route, Switch, useHistory } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { Navigation } from './components'
 import { useDispatch } from 'react-redux'
 import * as sessionActions from "./store/session";
@@ -7,35 +7,12 @@ import { Home, NewSpotForm, ManageSpots, UpdateSpot, ManageReviews } from "./pag
 import Spot from "./pages/Spot/Spot";
 
 function App() {
-  const history = useHistory();
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
-
-  const handleRouteChange = () => {
-    const root = document.getElementById('root');
-    root.classList.remove('spots-page');
-
-    const spotPageRegex = /^\/spots\/\d+$/; // Regular expression to match "/spots/" followed by one or more digits
-
-    if (spotPageRegex.test(history.location.pathname)) {
-      root.classList.add('spots-page');
-    }
-  };
-
-  useEffect(() => {
-    handleRouteChange();
-
-    const unlisten = history.listen(() => {
-      handleRouteChange();
-    });
-
-    return () => unlisten();
-  });
-
 
   return (
     <>
